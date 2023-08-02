bd74789f21ab7436a4a6be96dbc7cf017e674a18
anomula25 <57620742+anomula25@users.noreply.github.com>
2023-08-02 16:51:08 -0400

    Update main.py

--- VV/main.py
+++ VV/main.py
@@ -16,7 +16,7 @@ app = Flask(__name__, template_folder='templates')
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root123'
+app.config['MYSQL_PASSWORD'] = '*******'
 app.config['MYSQL_DB'] = 'signlanguage'
 
 mysql = MySQL(app)
